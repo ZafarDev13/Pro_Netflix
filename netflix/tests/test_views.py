0a64f729579a81051085069273d9f2b70c11e4eb
Zafar <you@example.com>
2022-05-08 23:19:15 +0500

    update 8 test_views.py github actions

--- netflix/tests/test_views.py
+++ netflix/tests/test_views.py
@@ -13,28 +13,28 @@ class TestMovieViewSet(TestCase):
         response = self.client.get('/v1/movies/')
         data = response.data
 
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(data), 2)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 2)
         self.assertIsNotNone(data[0]["id"])
-        self.assertEquals(data[0]['name'], "Test1 movie name")
-        self.assertEquals(data[0]['year'], "2022-12-12")
-        self.assertEquals(data[0]['imdb'], 1)
-        self.assertEquals(data[0]['genre'], "Drama")
-        self.assertEquals(data[0]['actors'], [])
+        self.assertEqual(data[0]['name'], "Test1 movie name")
+        self.assertEqual(data[0]['year'], "2022-12-12")
+        self.assertEqual(data[0]['imdb'], 1)
+        self.assertEqual(data[0]['genre'], "Drama")
+        self.assertEqual(data[0]['actors'], [])
 
 
     def test_search(self):
         response = self.client.get('/v1/movies/?search=Test')
         data = response.data
 
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(len(data), 2)
-        self.assertEquals(data[0]['name'], "Test1 movie name")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(data), 2)
+        self.assertEqual(data[0]['name'], "Test1 movie name")
 
     def test_ordering(self):
         response = self.client.get('/v1/movies/?ordering=-imdb')
         data = response.data
 
-        self.assertEquals(response.status_code, 200)
-        self.assertEquals(data[0]['imdb'], 5)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data[0]['imdb'], 5)
         print(f"**********{data}")
